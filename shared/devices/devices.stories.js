347424aaa73298e1540529cfb864a950f17f0160
chrisnojima <cnojima@keyba.se>
2017-07-24 17:13:03 -0400

    WIP

---
+++ shared/devices/devices.stories.js
@@ -0,0 +1,20 @@
+// @flow
+import React from 'react'
+
+const load = () => {
+  storiesOf('Devices', module).add('List', () => {
+    // const dev1 = new DeviceDetailRecord({
+    // created: 1444423192000,
+    // currentDevice: false,
+    // deviceID: '3d2e716574f68fb4eb5b56a5ec8eeeee',
+    // lastUsed: 1444423192001,
+    // name: 'Paper Key (lorem ipsum...)',
+    // provisionedAt: 1444423192000,
+    // provisioner: null,
+    // revokedAt: null,
+    // type: 'backup',
+    // })
+  })
+}
+
+export default load
